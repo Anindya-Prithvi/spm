dfcf2b54474ca4444cda692373c3b091ff1c1cb4
Andy Candy <anindya20024@iiitd.ac.in>
2022-08-06 23:58:10 +0530

    Fully functional

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use encdfun::inits;
 use encdfun::verifypass::key_creator;
+use encdfun::{decfuns::read_password, encfuns::write_password};
 use openssl::symm::Cipher;
 use std::{env, fs, path};
 pub mod encdfun;
@@ -7,7 +8,6 @@ pub mod encdfun;
 fn main() {
     let mut basepath: path::PathBuf = env::current_exe().unwrap();
     basepath.pop();
-    println!("{}", basepath.display());
     let cipher = Cipher::aes_256_cbc();
     let iv = b"watashiwa kyojin"; // or change acc your needs, doesn't matter
     println!("Welcome to post pwn clarity");
@@ -28,21 +28,55 @@ fn main() {
     };
 
     let args: Vec<String> = env::args().collect();
-    if args.len() > 1 {
+    let totalargs = args.len();
+    if totalargs > 1 {
         match args[1].as_str() {
-            "read" => {
+            "--read" | "-r" => {
                 println!("Reading mode");
+                if totalargs != 3 {
+                    println!("Invalid syntax: Correction --> spm --read <identifier>");
+                    return;
+                }
+                read_password(args[2].as_str(), &keyhash, &cipher, iv, &basepath);
+                return;
             }
-            "write" => {
-                println!("Writing mode");
+            "--write" | "-w" => {
+                println!("Writing mode. Spaces in password disallowed");
+                if totalargs != 4 {
+                    println!("Invalid syntax: Correction --> spm --write <identifier> <password>");
+                    return;
+                }
+                write_password(args[2].as_str(), &args[3], &keyhash, &cipher, iv, &basepath);
+                return;
+            }
+            "--help" | "-h" => {
+                println!(
+                    "\
+                    Usage: To view help menu\n\
+                    \tspm -h\n\
+                    \tspm --help\n\
+                    Usage: To read directly (non interactive)\n\
+                    \tspm --read <identifier>\n\
+                    \tspm -r <identifier>\n\
+                    Usage: To write directly (non interactive)\n\
+                    \tspm --write <identifier> <password>\n\
+                    \tspm -w <identifier> <password>\n\
+                    For additional security, you can delete {}/key.txt\n\
+                    but be SURE to keep the creation(Master) password same",
+                    basepath.display()
+                );
             }
             _ => {
-                println!("Interactive mode --xxxx--");
+                println!("Invalid options\n Falling back to Interactive mode:");
                 loop {
                     inits::interact(&basepath, &keyhash, &cipher, iv);
                 }
             }
         }
-        println!("{:?}", args);
+    } else {
+        println!("Welcome to interactive mode");
+        loop {
+            inits::interact(&basepath, &keyhash, &cipher, iv);
+        }
     }
 }
