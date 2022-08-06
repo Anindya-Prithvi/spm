a06687ac7d605f50c9d1f138ba49be246d56b10d
Andy Candy <anindya20024@iiitd.ac.in>
2022-08-06 13:43:22 +0530

    read pass and vfy

--- src/main.rs
+++ src/main.rs
@@ -21,9 +21,53 @@ fn main() {
             keycreator();
         }
     };
-    println!("keyhash: {:?}", keyhash);
-    verifyPassword(keyhash).unwrap();
+    // println!("keyhash: {:?}", keyhash);
+    // verifyPassword(keyhash).unwrap();
+    println!("## Entering infinite loop");
     // add function to add passwords
+    loop {
+        println!("1. Read password\n2. Create Password\nEnter 1 or 2: ");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        match input.trim() {
+            "1" => {
+                let passdir =
+                    fs::read_dir("./dnames").unwrap_or_else(|e| panic!("No passwords saved"));
+                let mut totalpass = 1;
+                for file in passdir {
+                    println!("1. {}", file.unwrap().path().display());
+                    totalpass += 1;
+                }
+                if totalpass == 1 {
+                    println!("No passwords saved");
+                    continue;
+                }
+
+                println!("Enter identifying name (domain) of password to read");
+                let mut input = String::new();
+                io::stdin().read_line(&mut input).unwrap();
+                readpassword(input.trim(), &keyhash, cipher, iv);
+            }
+            "2" => {
+                let success = match fs::read_dir("./dnames") {
+                    Ok(x) => true,
+                    Err(e) => {
+                        fs::create_dir("./dnames").unwrap();
+                        false
+                    }
+                };
+                println!("Enter password to create");
+                let mut input = String::new();
+                io::stdin().read_line(&mut input).unwrap();
+            }
+            _ => {
+                println!("Invalid input");
+            }
+        }
+    }
+}
+fn readpassword(name: &str, keyhash: &Vec<u8>, cipher: Cipher, iv: &[u8]) {
+
 }
 
 fn verifyPassword(keyhash: Vec<u8>) -> Result<(), String> {
@@ -34,7 +78,7 @@ fn verifyPassword(keyhash: Vec<u8>) -> Result<(), String> {
         return Err("Password too long".to_string());
     }
     let passwordhash = hash(MessageDigest::sha512(), passwordpadded.as_bytes()).unwrap();
-    let mut matched:bool = true;
+    let mut matched: bool = true;
     passwordhash.iter().zip(keyhash.iter()).for_each(|(x, y)| {
         matched = matched && (x == y);
     });
@@ -57,13 +101,15 @@ fn keycreator() {
         println!("Password is too long");
         return;
     }
-    fs::write("key.txt", hash(MessageDigest::sha512(), key.as_bytes()).unwrap()).unwrap();
+    fs::write(
+        "key.txt",
+        hash(MessageDigest::sha512(), key.as_bytes()).unwrap(),
+    )
+    .unwrap();
     println!("Key creation successful");
 }
 
-fn dmain() {
-
-}
+fn dmain() {}
 
 fn decrypter(data: &Vec<u8>, key: &[u8], iv: &[u8]) {
     let cipher = Cipher::aes_128_cbc();
